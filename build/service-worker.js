0e577d11a8bdd115f8ce6829d6e96de9cb144435
Andrew Cottam <a.cottam@gmail.com>
2020-03-24 11:29:37 +0000

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f78c2bd93e80332684d1e4579f3d34b.js"
+  "/precache-manifest.b3500cf9de741a551d8c71c7605a822d.js"
 );
 
 self.addEventListener('message', (event) => {
