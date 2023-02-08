29cd67bc6edcbf09347f16a08ee2bb11d814e03d
Andrew Cottam <andrew@restor.eco>
2023-02-08 22:47:50 +0100

    new build after changing mapbox user

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ac2c5231a5a7422f742d54cb585625e.js"
+  "/precache-manifest.a9cb9806ab8b5676b1fe2cb9e67dab59.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
